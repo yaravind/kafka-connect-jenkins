2d39231a5aed9244466c2bb38be73dbbe10bd000
Aravind R Yarram <yaravind@gmail.com>
2016-05-07 20:03:33 -0400

    Add config class and unit tests for Source connector ref #2

---
+++ src/main/java/org/aravind/oss/kafka/connector/JenkinsSourceConfig.java
@@ -0,0 +1,77 @@
+package org.aravind.oss.kafka.connector;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * Convenient class to hold configuration properties of the JenkinsSourceConnector
+ *
+ * @author Aravind R Yarram
+ * @since 0.5
+ */
+public class JenkinsSourceConfig extends AbstractConfig {
+
+    public static final String JENKINS_BASE_URL_CONFIG = "jenkins.base.url";
+    private static final String JENKINS_BASE_URL_DOC = "This is the URL of the home page of your Jenkins installation. "
+            + "For e.g. https://builds.apache.org/ is the base url of the Apache Jenkins public instance. " +
+            "In some installations, a context/prefix might have been specified (using the --prefix; For e.g. --prefix=/jenkins). " +
+            "If so then the url should include the prefix as well. ";
+
+    public static final String JOBS_RESOURCE_PATH_CONFIG = "jenkins.jobs.resource.path";
+    private static final String JOBS_RESOURCE_PATH_DEFAULT = "/api/json";
+    private static final String JOBS_RESOURCE_PATH_DOC = "This is the REST resource path to retrieve all jobs defined in the Jenkins instance. " +
+            "This is an optional configuration property. If not specified the default \"/api/json\" will be used.";
+
+    public static final String JENKINS_USERNAME_CONFIG = "jenkins.username";
+    private static final String JENKINS_USERNAME_CONFIG_DOC = "Username to use when connecting to protected Jenkins.";
+
+    public static final String JENKINS_PASSWORD_OR_API_TOKEN_CONFIG = "jenkins.password.or.api.token";
+    private static final String JENKINS_PASSWORD_OR_API_TOKEN_DOC = "Password (or API Token) to use when connecting to protected Jenkins.";
+
+    private static final ConfigDef DEFS = new ConfigDef();
+
+    static {
+        DEFS
+                .define(JENKINS_BASE_URL_CONFIG, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, JENKINS_BASE_URL_DOC)
+                .define(JENKINS_USERNAME_CONFIG, ConfigDef.Type.STRING, "", ConfigDef.Importance.LOW, JENKINS_USERNAME_CONFIG_DOC)
+                .define(JENKINS_PASSWORD_OR_API_TOKEN_CONFIG, ConfigDef.Type.STRING, "", ConfigDef.Importance.LOW, JENKINS_PASSWORD_OR_API_TOKEN_DOC)
+                .define(JOBS_RESOURCE_PATH_CONFIG, ConfigDef.Type.STRING, JOBS_RESOURCE_PATH_DEFAULT, ConfigDef.Importance.LOW, JOBS_RESOURCE_PATH_DOC);
+    }
+
+    public JenkinsSourceConfig(Map<String, String> originals) {
+        super(DEFS, originals);
+    }
+
+    public URL getJenkinsUrl() {
+        try {
+            return new URL(getString(JENKINS_BASE_URL_CONFIG));
+        } catch (MalformedURLException e) {
+            throw new ConfigException("Couldn't create the URL from " + getString(JENKINS_BASE_URL_CONFIG), e);
+        }
+    }
+
+    public String getUsername() {
+        return getString(JENKINS_USERNAME_CONFIG);
+    }
+
+    public String getPasswordOrApiToken() {
+        return getString(JENKINS_PASSWORD_OR_API_TOKEN_CONFIG);
+    }
+
+    public boolean isProtected() {
+        return getString(JENKINS_USERNAME_CONFIG) != null && !getString(JENKINS_USERNAME_CONFIG).isEmpty();
+    }
+
+    public URL getJobsResource() {
+        try {
+            return new URL(getString(JENKINS_BASE_URL_CONFIG) + JOBS_RESOURCE_PATH_DEFAULT);
+        } catch (MalformedURLException e) {
+            throw new ConfigException("Couldn't create the URL from " + getString(JENKINS_BASE_URL_CONFIG), e);
+        }
+    }
+}
---
+++ src/test/groovy/org/aravind/oss/kafka/connector/JenkinsSourceConfigTest.groovy
@@ -0,0 +1,102 @@
+package org.aravind.oss.kafka.connector
+
+import org.apache.kafka.common.config.ConfigException
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * @author Aravind R Yarram
+ * @since 0.5.0
+ */
+class JenkinsSourceConfigTest extends Specification {
+
+    @Shared
+    def Map cfg
+
+    def setupSpec() {
+        cfg = ['jenkins.base.url'             : 'https://builds.apache.org',
+               'jenkins.username'             : 'myuser',
+               'jenkins.password.or.api.token': 'mypassword',
+               'jenkins.jobs.resource.path'   : '/api/json']
+    }
+
+    //Error scenarios
+
+    def "Missing 'jenkins.base.url' property should throw exception"() {
+        when:
+        def props = [:]
+        def jenkinsCfg = new JenkinsSourceConfig(props)
+
+        then:
+        thrown(ConfigException)
+    }
+
+    def "Default value of '/api/json' is chosen when 'jenkins.jobs.resource.path' property is not specified"() {
+        when:
+        def props = ['jenkins.base.url': 'https://builds.apache.org']
+        def jenkinsCfg = new JenkinsSourceConfig(props)
+
+        then:
+        jenkinsCfg.getJobsResource() == new URL('https://builds.apache.org/api/json')
+    }
+
+    def "Not starting the 'jenkins.jobs.resource.path' property value with '/' should throw an exception"() {
+        when:
+        def props = ['jenkins.jobs.resource.path': 'api/json']
+        def jenkinsCfg = new JenkinsSourceConfig(props)
+
+        then:
+        thrown(ConfigException)
+    }
+
+    //Happy scenarios
+
+    def "'jenkins.base.url' is the only REQUIRED property"() {
+        when:
+        def props = ['jenkins.base.url': 'https://builds.apache.org']
+        def jenkinsCfg = new JenkinsSourceConfig(props)
+
+        then:
+        notThrown(ConfigException)
+    }
+
+    def "GetJenkinsUrl"() {
+        when:
+        def jenkinsCfg = new JenkinsSourceConfig(cfg)
+
+        then: "an URL object is returned"
+        jenkinsCfg.getJenkinsUrl() == new URL('https://builds.apache.org')
+    }
+
+    def "GetUsername"() {
+        when:
+        def jenkinsCfg = new JenkinsSourceConfig(cfg)
+
+        then:
+        jenkinsCfg.getUsername() == 'myuser'
+    }
+
+    def "GetPasswordOrApiToken"() {
+        when:
+        def jenkinsCfg = new JenkinsSourceConfig(cfg)
+
+        then:
+        jenkinsCfg.getPasswordOrApiToken() == 'mypassword'
+    }
+
+    def "IsProtected"() {
+        when:
+        def jenkinsCfg = new JenkinsSourceConfig(cfg)
+
+        then:
+        jenkinsCfg.isProtected() == true
+    }
+
+    def "GetJobsResource"() {
+        when:
+        def jenkinsCfg = new JenkinsSourceConfig(cfg)
+
+        then:
+        jenkinsCfg.getJobsResource() == new URL('https://builds.apache.org/api/json')
+    }
+}
