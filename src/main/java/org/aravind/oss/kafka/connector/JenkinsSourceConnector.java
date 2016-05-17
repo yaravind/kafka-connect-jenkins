27040bdb89d0b3475974dca54ed8e3a689fbf93b
Aravind R Yarram <yaravind@gmail.com>
2016-05-16 20:48:03 -0400

    Add static task config split support #2

--- src/main/java/org/aravind/oss/kafka/connector/JenkinsSourceConnector.java
+++ src/main/java/org/aravind/oss/kafka/connector/JenkinsSourceConnector.java
@@ -1,8 +1,105 @@
 package org.aravind.oss.kafka.connector;
 
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.errors.ConnectException;
+import org.apache.kafka.connect.source.SourceConnector;
+import org.apache.kafka.connect.util.ConnectorUtils;
+import org.aravind.oss.jenkins.JenkinsException;
+import org.aravind.oss.jenkins.JenkinsClient;
+import org.aravind.oss.jenkins.domain.Jenkins;
+import org.aravind.oss.jenkins.domain.Job;
+
+import java.net.HttpURLConnection;
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
+ * Is responsible for breaking the job into a set of tasks  that can be distributed to workers.
+ *
  * @author Aravind R Yarram
- * @since <<add version>>
+ * @since 0.5.0
  */
-public class JenkinsSourceConnector {
+public class JenkinsSourceConnector extends SourceConnector {
+    private JenkinsSourceConfig jenkinsCfg;
+    private JenkinsClient client;
+
+    @Override
+    public String version() {
+        return Version.get();
+    }
+
+    /**
+     * Start this Connector. This method will only be called on a clean Connector, i.e. it has either just been
+     * instantiated and initialized or stop() has been invoked.
+     *
+     * @param props
+     */
+    @Override
+    public void start(Map<String, String> props) {
+        jenkinsCfg = new JenkinsSourceConfig(props);
+
+        //Initialize connection to Jenkins instance
+        try {
+            client = new JenkinsClient(jenkinsCfg.getJobsResource());
+
+            //Do a test connection to Fail Fast
+            HttpURLConnection connection = client.connect();
+            connection.disconnect();
+        } catch (JenkinsException e) {
+            throw new ConnectException("Unable to open connection to " + jenkinsCfg.getJenkinsUrl(), e);
+        }
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        return null;
+    }
+
+    /**
+     * Returns a set of configurations for {@link JenkinsSourceTask} based on the current configuration, producing at most {@code numTasks} configurations.
+     *
+     * @param maxTasks maximum number of configurations to generate
+     * @return configurations for Tasks
+     */
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        Optional<Jenkins> resp = null;
+        try {
+            resp = client.getJenkins();
+        } catch (JenkinsException e) {
+            //TODO sometimes the client might have been brought down. Let us handle it silently for now.
+            e.printStackTrace();
+        }
+
+        if (resp.isPresent()) {
+            Jenkins jenkins = resp.get();
+
+            //Create job groups
+            int jobsCount = jenkins.getJobs().size();
+            int numGroups = Math.min(jobsCount, maxTasks);
+            List<List<Job>> jobGroups = ConnectorUtils.groupPartitions(jenkins.getJobs(), numGroups);
+
+            //Create task configs for each group
+            List<Map<String, String>> taskConfigs = new ArrayList<>(jobGroups.size());
+
+            for (List<Job> group : jobGroups) {
+                Map<String, String> taskProps = new HashMap<>();
+
+                //Concatenate all job urls that need to be handled by a single task
+                String commaSeparatedJobUrls = group.stream()
+                        .map(j -> j.getUrl())
+                        .collect(Collectors.joining(","));
+
+                taskProps.put(JenkinsSourceTask.JOB_URLS, commaSeparatedJobUrls);
+
+                taskConfigs.add(taskProps);
+            }
+            return taskConfigs;
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void stop() {
+    }
 }
--- src/test/groovy/org/aravind/oss/kafka/connector/JenkinsSourceConnectorTest.groovy
+++ src/test/groovy/org/aravind/oss/kafka/connector/JenkinsSourceConnectorTest.groovy
@@ -1,18 +1,189 @@
 package org.aravind.oss.kafka.connector
 
+import org.apache.kafka.connect.connector.ConnectorContext
+import org.apache.kafka.connect.errors.ConnectException
+import spock.lang.Shared
 import spock.lang.Specification
+
+import static com.github.dreamhead.moco.Moco.pathResource
+import static com.github.dreamhead.moco.MocoJsonRunner.jsonHttpServer
+import static com.github.dreamhead.moco.Runner.runner
 
 /**
  * @author Aravind R Yarram
- * @since <<add version>>
+ * @since 0.5.0
  */
 class JenkinsSourceConnectorTest extends Specification {
-    def "test taskConfigs"() {
+    @Shared
+    ConnectorContext context = Mock()
+
+    Map sourceProps = [:]
+    JenkinsSourceConnector connector
+
+    def setup() {
+        connector = new JenkinsSourceConnector();
+        connector.initialize(context)
+    }
+
+    def cleanup() {
+        if (connector != null) connector.stop()
+    }
+    //Error scenarios
+
+    def "Config - Wrong jenkins url should throw exception"() {
+        when:
+        sourceProps.put(JenkinsSourceConfig.JENKINS_BASE_URL_CONFIG, 'https://wrong.domain.name')
+        connector.start(sourceProps)
+
+        then:
+        thrown(ConnectException)
+    }
+
+    //Happy scenarios
+
+    def "Config - Correct config should start the connector"() {
         given:
+        def server = jsonHttpServer(8181, pathResource("jenkins-mock-server-cfg.json"))
+        def mock = runner(server)
+        mock.start()
 
         when:
-        // TODO implement stimulus
+        sourceProps.put(JenkinsSourceConfig.JENKINS_BASE_URL_CONFIG, 'http://localhost:8181')
+        connector.start(sourceProps)
+
         then:
-        // TODO implement assertions
+        noExceptionThrown()
+    }
+
+    def "Partitioning - Single job"() {
+        given:
+        def server = jsonHttpServer(9292, pathResource("jenkins-mock-server-single-job-cfg.json"))
+        def mock = runner(server)
+        mock.start()
+
+        def MAX_TASKS_IS_ONE = 1
+
+        when:
+        sourceProps.put(JenkinsSourceConfig.JENKINS_BASE_URL_CONFIG, 'http://localhost:9292')
+        connector.start(sourceProps)
+
+        def taskCfgs = connector.taskConfigs(MAX_TASKS_IS_ONE)
+
+        then:
+        taskCfgs.size() == MAX_TASKS_IS_ONE
+
+        Map<String, String> cfg = taskCfgs.get(0)
+        cfg != null
+        cfg[JenkinsSourceTask.JOB_URLS] == 'https://builds.apache.org/job/Abdera-trunk/'
+
+        mock.stop()
+    }
+
+    def "Partitioning - Multiple jobs"() {
+        given:
+        def server = jsonHttpServer(9293, pathResource("jenkins-mock-server-three-job-cfg.json"))
+        def mock = runner(server)
+        mock.start()
+
+        def MAX_TASKS_IS_ONE = 1
+
+        when:
+        sourceProps.put(JenkinsSourceConfig.JENKINS_BASE_URL_CONFIG, 'http://localhost:9293')
+        connector.start(sourceProps)
+
+        def taskCfgs = connector.taskConfigs(MAX_TASKS_IS_ONE)
+
+        then:
+        taskCfgs.size() == MAX_TASKS_IS_ONE
+
+        Map<String, String> cfg = taskCfgs.get(0)
+        cfg != null
+        cfg[JenkinsSourceTask.JOB_URLS] == 'https://builds.apache.org/job/Abdera-trunk/,https://builds.apache.org/job/Accumulo-1.8/,https://builds.apache.org/job/Allura/'
+
+        mock.stop()
+    }
+
+    def "Partitioning - Multiple jobs with 2 MAX_TASKS"() {
+        given:
+        def server = jsonHttpServer(9293, pathResource("jenkins-mock-server-three-job-cfg.json"))
+        def mock = runner(server)
+        mock.start()
+
+        def MAX_TASKS_IS_TWO = 2
+
+        when:
+        sourceProps.put(JenkinsSourceConfig.JENKINS_BASE_URL_CONFIG, 'http://localhost:9293')
+        connector.start(sourceProps)
+
+        def taskCfgs = connector.taskConfigs(MAX_TASKS_IS_TWO)
+
+        then:
+        taskCfgs.size() == MAX_TASKS_IS_TWO
+
+        Map<String, String> cfg = taskCfgs.get(0)
+        cfg != null
+        cfg[JenkinsSourceTask.JOB_URLS] == 'https://builds.apache.org/job/Abdera-trunk/,https://builds.apache.org/job/Accumulo-1.8/'
+
+        Map<String, String> cfg1 = taskCfgs.get(1)
+        cfg1 != null
+        cfg1[JenkinsSourceTask.JOB_URLS] == 'https://builds.apache.org/job/Allura/'
+
+        mock.stop()
+    }
+
+    def "Partitioning - Multiple jobs with 3 MAX_TASKS"() {
+        given:
+        def server = jsonHttpServer(9293, pathResource("jenkins-mock-server-three-job-cfg.json"))
+        def mock = runner(server)
+        mock.start()
+
+        def MAX_TASKS_IS_THREE = 3
+
+        when:
+        sourceProps.put(JenkinsSourceConfig.JENKINS_BASE_URL_CONFIG, 'http://localhost:9293')
+        connector.start(sourceProps)
+
+        def taskCfgs = connector.taskConfigs(MAX_TASKS_IS_THREE)
+
+        then:
+        taskCfgs.size() == MAX_TASKS_IS_THREE
+
+        Map<String, String> cfg = taskCfgs.get(0)
+        cfg != null
+        cfg[JenkinsSourceTask.JOB_URLS] == 'https://builds.apache.org/job/Abdera-trunk/'
+
+        Map<String, String> cfg1 = taskCfgs.get(1)
+        cfg1 != null
+        cfg1[JenkinsSourceTask.JOB_URLS] == 'https://builds.apache.org/job/Accumulo-1.8/'
+
+        Map<String, String> cfg2 = taskCfgs.get(2)
+        cfg2 != null
+        cfg2[JenkinsSourceTask.JOB_URLS] == 'https://builds.apache.org/job/Allura/'
+
+        mock.stop()
+    }
+
+    def "Partitioning - Less jobs and more MAX_TASKS"() {
+        given:
+        def server = jsonHttpServer(9294, pathResource("jenkins-mock-server-single-job-cfg.json"))
+        def mock = runner(server)
+        mock.start()
+
+        def MAX_TASKS_IS_THREE = 3
+
+        when:
+        sourceProps.put(JenkinsSourceConfig.JENKINS_BASE_URL_CONFIG, 'http://localhost:9294')
+        connector.start(sourceProps)
+
+        def taskCfgs = connector.taskConfigs(MAX_TASKS_IS_THREE)
+
+        then:
+        taskCfgs.size() == 1
+
+        Map<String, String> cfg = taskCfgs.get(0)
+        cfg != null
+        cfg[JenkinsSourceTask.JOB_URLS] == 'https://builds.apache.org/job/Abdera-trunk/'
+
+        mock.stop()
     }
 }
