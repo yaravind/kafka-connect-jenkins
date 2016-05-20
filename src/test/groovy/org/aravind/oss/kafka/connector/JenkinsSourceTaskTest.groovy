67dbb9671aa8d3c8ccf4c8bf54e155b7df8a2e0d
Aravind R Yarram <yaravind@gmail.com>
2016-05-20 19:44:27 -0400

    Add unit tests for multiple job urls as taskProps

--- src/test/groovy/org/aravind/oss/kafka/connector/JenkinsSourceTaskTest.groovy
+++ src/test/groovy/org/aravind/oss/kafka/connector/JenkinsSourceTaskTest.groovy
@@ -40,9 +40,21 @@ class JenkinsSourceTaskTest extends Specification {
         if (sourceTask != null) sourceTask.stop()
     }
 
-    def "test poll"() {
+    def "Should support single job url as taskProps"() {
         given:
         def taskProps = ['job.urls': 'http://localhost:8181/job/Abdera-trunk/']
+        sourceTask.start(taskProps)
+
+        when:
+        def sourceRecord = sourceTask.poll()
+
+        then:
+        sourceRecord == null
+    }
+
+    def "Should support multiple comma separated job urls as taskProps"() {
+        given:
+        def taskProps = ['job.urls': 'http://localhost:8181/job/Abdera-trunk/,https://builds.apache.org/job/Accumulo-1.8/']
         sourceTask.start(taskProps)
 
         when:
