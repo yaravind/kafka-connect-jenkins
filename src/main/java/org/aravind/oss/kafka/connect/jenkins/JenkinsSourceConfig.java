6397773bc4eb2ed6265948c57925ba35759a2bbe
Aravind R Yarram <yaravind@gmail.com>
2016-06-12 16:05:09 -0400

    Add config support #22

--- src/main/java/org/aravind/oss/kafka/connect/jenkins/JenkinsSourceConfig.java
+++ src/main/java/org/aravind/oss/kafka/connect/jenkins/JenkinsSourceConfig.java
@@ -17,6 +17,7 @@ import java.util.Map;
 public class JenkinsSourceConfig extends AbstractConfig {
 
     public static final String JENKINS_BASE_URL_CONFIG = "jenkins.base.url";
+    public static final String JENKINS_BASE_URL_DISPLAY = "Jenkins server url.";
     private static final String JENKINS_BASE_URL_DOC = "This is the URL of the home page of your Jenkins installation. "
             + "For e.g. https://builds.apache.org/ is the base url of the Apache Jenkins public instance. " +
             "In some installations, a context/prefix might have been specified (using the --prefix; For e.g. --prefix=/jenkins). " +
@@ -24,41 +25,56 @@ public class JenkinsSourceConfig extends AbstractConfig {
 
     public static final String JOBS_RESOURCE_PATH_CONFIG = "jenkins.jobs.resource.path";
     private static final String JOBS_RESOURCE_PATH_DEFAULT = "/api/json";
+    private static final String JOBS_RESOURCE_PATH_DISPLAY = "Path to API. Default (/api/json)";
     private static final String JOBS_RESOURCE_PATH_DOC = "This is the REST resource path to retrieve all jobs defined in the Jenkins instance. " +
             "This is an optional configuration property. If not specified the default \"/api/json\" will be used.";
 
     public static final String JENKINS_USERNAME_CONFIG = "jenkins.username";
+    public static final String JENKINS_USERNAME_DISPLAY = "Jenkins Username.";
     private static final String JENKINS_USERNAME_CONFIG_DOC = "Username to use when connecting to protected Jenkins.";
 
     public static final String JENKINS_PASSWORD_OR_API_TOKEN_CONFIG = "jenkins.password.or.api.token";
+    public static final String JENKINS_PASSWORD_OR_API_TOKEN_DISPLAY = "Password (or API Token)";
     private static final String JENKINS_PASSWORD_OR_API_TOKEN_DOC = "Password (or API Token) to use when connecting to protected Jenkins.";
 
     public static final String JENKINS_CONN_TIMEOUT_CONFIG = "jenkins.connection.timeoutInMillis";
+    public static final String JENKINS_CONN_TIMEOUT_DISPLAY = "Connection timeout in milliseconds.";
     public static final int JENKINS_CONN_TIMEOUT_DEFAULT = 500;
     public static final String JENKINS_CONN_TIMEOUT_DOC = "Connection timeout in milliseconds. " +
             "This denotes the time elapsed before the connection established or Server responded to connection request.";
 
     public static final String JENKINS_READ_TIMEOUT_CONFIG = "jenkins.read.timeoutInMillis";
+    public static final String JENKINS_READ_TIMEOUT_DISPLAY = "Response read timeout in milliseconds.";
     public static final int JENKINS_READ_TIMEOUT_DEFAULT = 3000;
     public static final String JENKINS_READ_TIMEOUT_DOC = "Response read timeout in milliseconds. After establishing the connection, " +
             "the client socket waits for response after sending the request. " +
             "This is the elapsed time since the client has sent request to the server before server responds.";
 
     public static final String TOPIC_CONFIG = "topic";
+    public static final String TOPIC_DISPLAY = "Topic to persist build events.";
     public static final String TOPIC_CONFIG_DOC = "This is the name of the Kafka Topic to which the source records containing Jenkins Build details are written to.";
     public static final String TOPIC_CONFIG_DEFAULT = "jenkins.connector.topic";
+
+    public static final String JENKINS_POLL_INTERVAL_MS_CONFIG = "jenkins.pollIntervalInMillis";
+    private static final String JENKINS_POLL_INTERVAL_MS_DOC = "Frequency in ms to poll for new build events in Jenkins.";
+    public static final int JENKINS_POLL_INTERVAL_MS_DEFAULT = 60000;//every minute
+    private static final String JENKINS_POLL_INTERVAL_MS_DISPLAY = "Poll Interval in milliseconds";
+
+    public static final String JENKINS_GROUP = "Jenkins";
+    public static final String CONNECTOR_GROUP = "Connector";
 
     public static final ConfigDef DEFS = new ConfigDef();
 
     static {
         DEFS
-                .define(JENKINS_BASE_URL_CONFIG, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, JENKINS_BASE_URL_DOC)
-                .define(JENKINS_USERNAME_CONFIG, ConfigDef.Type.STRING, "", ConfigDef.Importance.LOW, JENKINS_USERNAME_CONFIG_DOC)
-                .define(JENKINS_PASSWORD_OR_API_TOKEN_CONFIG, ConfigDef.Type.STRING, "", ConfigDef.Importance.LOW, JENKINS_PASSWORD_OR_API_TOKEN_DOC)
-                .define(JENKINS_CONN_TIMEOUT_CONFIG, ConfigDef.Type.INT, JENKINS_CONN_TIMEOUT_DEFAULT, ConfigDef.Importance.LOW, JENKINS_CONN_TIMEOUT_DOC)
-                .define(JENKINS_READ_TIMEOUT_CONFIG, ConfigDef.Type.INT, JENKINS_READ_TIMEOUT_DEFAULT, ConfigDef.Importance.LOW, JENKINS_READ_TIMEOUT_DOC)
-                .define(JOBS_RESOURCE_PATH_CONFIG, ConfigDef.Type.STRING, JOBS_RESOURCE_PATH_DEFAULT, ConfigDef.Importance.LOW, JOBS_RESOURCE_PATH_DOC)
-                .define(TOPIC_CONFIG, ConfigDef.Type.STRING, TOPIC_CONFIG_DEFAULT, ConfigDef.Importance.LOW, TOPIC_CONFIG_DOC);
+                .define(JENKINS_BASE_URL_CONFIG, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, JENKINS_BASE_URL_DOC, JENKINS_GROUP, 1, ConfigDef.Width.LONG, JENKINS_BASE_URL_DISPLAY)
+                .define(JENKINS_USERNAME_CONFIG, ConfigDef.Type.STRING, "", ConfigDef.Importance.LOW, JENKINS_USERNAME_CONFIG_DOC, JENKINS_GROUP, 5, ConfigDef.Width.MEDIUM, JENKINS_USERNAME_DISPLAY)
+                .define(JENKINS_PASSWORD_OR_API_TOKEN_CONFIG, ConfigDef.Type.STRING, "", ConfigDef.Importance.LOW, JENKINS_PASSWORD_OR_API_TOKEN_DOC, JENKINS_GROUP, 6, ConfigDef.Width.MEDIUM, JENKINS_PASSWORD_OR_API_TOKEN_DISPLAY)
+                .define(JENKINS_CONN_TIMEOUT_CONFIG, ConfigDef.Type.INT, JENKINS_CONN_TIMEOUT_DEFAULT, ConfigDef.Importance.LOW, JENKINS_CONN_TIMEOUT_DOC, JENKINS_GROUP, 3, ConfigDef.Width.SHORT, JENKINS_CONN_TIMEOUT_DISPLAY)
+                .define(JENKINS_READ_TIMEOUT_CONFIG, ConfigDef.Type.INT, JENKINS_READ_TIMEOUT_DEFAULT, ConfigDef.Importance.LOW, JENKINS_READ_TIMEOUT_DOC, JENKINS_GROUP, 4, ConfigDef.Width.SHORT, JENKINS_READ_TIMEOUT_DISPLAY)
+                .define(JOBS_RESOURCE_PATH_CONFIG, ConfigDef.Type.STRING, JOBS_RESOURCE_PATH_DEFAULT, ConfigDef.Importance.LOW, JOBS_RESOURCE_PATH_DOC, JENKINS_GROUP, 7, ConfigDef.Width.MEDIUM, JOBS_RESOURCE_PATH_DISPLAY)
+                .define(JENKINS_POLL_INTERVAL_MS_CONFIG, ConfigDef.Type.LONG, JENKINS_POLL_INTERVAL_MS_DEFAULT, ConfigDef.Importance.LOW, JENKINS_POLL_INTERVAL_MS_DOC, JENKINS_GROUP, 2, ConfigDef.Width.SHORT, JENKINS_POLL_INTERVAL_MS_DISPLAY)
+                .define(TOPIC_CONFIG, ConfigDef.Type.STRING, TOPIC_CONFIG_DEFAULT, ConfigDef.Importance.LOW, TOPIC_CONFIG_DOC, CONNECTOR_GROUP, 1, ConfigDef.Width.LONG, TOPIC_DISPLAY);
     }
 
     public JenkinsSourceConfig(Map<String, String> originals) {
